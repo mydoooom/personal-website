6dc8135733ac6250226c0af0715e95b8698712bd
Tran Viet Anh <tran@vietanh.cz>
2024-02-17 11:36:15 +0100

    Use npm instead of bun for Tailwind IntelliJ plugin to work

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,15 @@
 export const Hero = () => {
   return (
     <>
-      <div className="h-screen grid grid-cols-[1fr_3fr] items-center prose">
-        <p>Hello from</p>
-        <h1> Tran Viet Anh</h1>
+      <div className="h-screen flex items-center">
+        <div className="grid grid-cols-[1fr_3fr] items-start gap-10 prose">
+          <p>Hi! My name is</p>
+          <div className="flex flex-col">
+            <h1 className="m-0"> Tran Viet Anh</h1>
+            <em>Web Developer</em>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
