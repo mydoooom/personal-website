3363a3f59d4d0023a5c2811d838f45241f1d3186
Tran Viet Anh <tran@vietanh.cz>
2024-02-18 00:44:25 +0100

    Add colour accent for surname

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 
 export const Hero = () => {
+    const locale = useLocale();
     const t = useTranslations('Index');
 
     return (
@@ -9,7 +10,10 @@ export const Hero = () => {
                 <div className="grid grid-cols-1 md:grid-cols-[1fr_3fr] items-start gap-5 prose prose-slate dark:prose-invert max-w-none">
                     <strong>{t('hello')}</strong>
                     <div className="flex flex-col">
-                        <h1 className="m-0 mb-2 text-5xl md:text-7xl animate-slide-in-left">{t('name')}</h1>
+                        <h1 className="m-0 mb-2 text-5xl md:text-7xl animate-slide-in-left">
+                            <span className="text-teal-600 dark:text-teal-500">{t('surname')}</span>
+                            {`${locale !== 'zh' ? ' ' : ''}${t('name')}`}
+                        </h1>
                         <em className="pl-1">👨🏻‍💻 {t('web-developer')}</em>
                     </div>
                 </div>
