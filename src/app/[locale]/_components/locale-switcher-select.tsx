6c31cc48da53a199cfd4708d01cdb55b7143a7ef
Tran Viet Anh <tran@vietanh.cz>
2025-06-15 12:50:07 +0200

    Change select width

--- src/app/[locale]/_components/locale-switcher-select.tsx
+++ src/app/[locale]/_components/locale-switcher-select.tsx
@@ -31,7 +31,7 @@ export default function LocaleSwitcherSelect({
 
   return (
     <Select defaultValue={defaultValue} onValueChange={onSelectChange}>
-      <SelectTrigger className="w-36">
+      <SelectTrigger className="w-32">
         <SelectValue placeholder={label} />
       </SelectTrigger>
       <SelectContent>
